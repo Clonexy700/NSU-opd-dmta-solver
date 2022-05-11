e33690274b1dfb2f666ca25acf2288ef739f6a5b
Clonexy <conexy700@gmail.com>
2022-05-11 19:31:14 +0700

    e2e2 test

--- cypress/integration/simple_fsm.js
+++ cypress/integration/simple_fsm.js
@@ -1,16 +1,16 @@
 describe('Build basic FSS', () => {
-  it('visit app', () => {
-    cy.visit('http://localhost:5000');
-  });
+    it('visit app', () => {
+        cy.visit('http://localhost:5000/stateMachine');
+    });
 
-  it('build state', () => {
-  	cy.get('canvas').dblclick();
-  	cy.get('canvas').click({shiftKey : true});
-  	cy.contains('Draw Table').click();
-  	//
-  	cy.get('.t_tbl').invoke('text').then((text) => {
-  		//State S0, on input "" outputs "" and goes to S0
-  		expect(text.replace(/(\r\n|\n|\r|\s)/gm, "") ) .equal('S0εS0');
-  	});
-  });
+    it('build state', () => {
+        cy.get('canvas').dblclick();
+        cy.get('canvas').click({ shiftKey: true });
+        cy.contains('Draw Table').click();
+        //
+        cy.get('.t_tbl').invoke('text').then((text) => {
+            //State S0, on input "" outputs "" and goes to S0
+            expect(text.replace(/(\r\n|\n|\r|\s)/gm, "")).equal('S0εS0');
+        });
+    });
 });

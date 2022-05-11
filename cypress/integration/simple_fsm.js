977e60721441730181c653a07425478cd2560dc0
Clonexy <conexy700@gmail.com>
2022-05-11 19:25:35 +0700

    e2e testing

---
+++ cypress/integration/simple_fsm.js
@@ -0,0 +1,16 @@
+describe('Build basic FSS', () => {
+  it('visit app', () => {
+    cy.visit('http://localhost:5000');
+  });
+
+  it('build state', () => {
+  	cy.get('canvas').dblclick();
+  	cy.get('canvas').click({shiftKey : true});
+  	cy.contains('Draw Table').click();
+  	//
+  	cy.get('.t_tbl').invoke('text').then((text) => {
+  		//State S0, on input "" outputs "" and goes to S0
+  		expect(text.replace(/(\r\n|\n|\r|\s)/gm, "") ) .equal('S0εS0');
+  	});
+  });
+});

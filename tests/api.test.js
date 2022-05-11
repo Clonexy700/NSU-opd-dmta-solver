fe52fed9659f0f799e9ef7b0cca192695f146aee
Clonexy <conexy700@gmail.com>
2022-05-11 19:16:35 +0700

    chai test

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,4 +1,9 @@
-import {_API} from '../src/api.js';
+import { _API } from '../src/api.js';
+
+let chai = require("chai");
+let chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+let expect = chai.expect;
 
 var API = new _API();
 
@@ -7,9 +12,9 @@ beforeEach(() => {
 });
 
 test('basic test', () => {
-    let foo = function (response){ expect(response).toBe(2); } ;
+    let foo = function(response) { expect(response).toBe(2); };
 
-    API.addFunc( "test1" , foo);
+    API.addFunc("test1", foo);
     API.call("test1", 2);
 });
 
@@ -20,32 +25,32 @@ test('test empty', () => {
 
 
 test('test multiple funcs', () => {
-    let foo = function (response){ expect(response).toBe("test"); } ;
-    let bar = function (response){ expect(response).toBe("test"); } ;
-    let baz = function (response){ expect(response).toBe("test"); } ;
-        
-    API.addFunc( "a" , foo);
-    API.addFunc( "a" , bar);
-    API.addFunc( "a" , baz);
+    let foo = function(response) { expect(response).toBe("test"); };
+    let bar = function(response) { expect(response).toBe("test"); };
+    let baz = function(response) { expect(response).toBe("test"); };
+
+    API.addFunc("a", foo);
+    API.addFunc("a", bar);
+    API.addFunc("a", baz);
 
     API.call("a", "test");
 });
 
 
 test('test multiple args', () => {
-    let foo = function (a,b,c){ 
+    let foo = function(a, b, c) {
         expect(a).toBe(1);
         expect(b).toBe("foo");
-        expect(c).toBe(3.14); 
-    } ;
+        expect(c).toBe(3.14);
+    };
 
     API.addFunc("test", foo);
-    API.call("test", 1, "foo", 3.14 );
+    API.call("test", 1, "foo", 3.14);
 });
 
 
 test('test return', () => {
-    let foo = function(){ return 1};
+    let foo = function() { return 1 };
 
     API.addFunc("test", foo);
     expect(API.call("test")).toStrictEqual([1]);
@@ -53,28 +58,28 @@ test('test return', () => {
 
 
 test('test multiple return', () => {
-    let a = function(){ return 1};
-    let b = function(){ return "test" };
-    let c = function(){ return {} };
-    let d = function(x){ return x * 2 };
+    let a = function() { return 1 };
+    let b = function() { return "test" };
+    let c = function() { return {} };
+    let d = function(x) { return x * 2 };
 
     API.addFunc("test", a);
     API.addFunc("test", b);
     API.addFunc("test", c);
     API.addFunc("test", d);
 
-    expect(API.call("test", 2)).toStrictEqual([1,"test", {}, 4]);
+    expect(API.call("test", 2)).toStrictEqual([1, "test", {}, 4]);
 });
 
 
 test('test API to string', () => {
-    let a = function(){ return 1 };
-    
-    API.addFunc("test", a); 
-    API.addFunc("test", () => {return "str";}); 
-    API.addFunc("test_other", () => {return "str2";}); 
+    let a = function() { return 1 };
 
-    let expected =`test ==> 
+    API.addFunc("test", a);
+    API.addFunc("test", () => { return "str"; });
+    API.addFunc("test_other", () => { return "str2"; });
+
+    let expected = `test ==> 
       "function () {
           return 1;
         },() => {
@@ -83,8 +88,8 @@ test('test API to string', () => {
       test_other ==> 
       "() => {
           return "str2";
-        }"`.replace(/\n|\t|\s/g,"")
+        }"`.replace(/\n|\t|\s/g, "")
 
-    let test = API.dump(false).replace(/\n|\t|\s/g,"");
+    let test = API.dump(false).replace(/\n|\t|\s/g, "");
     expect(test).toBe(expected);
 });
--- tests/graph.test.js
+++ tests/graph.test.js
@@ -1,168 +1,173 @@
-import {Graph, save, load} from '../src/lib/graph.js';
-import {buildFakeCanvas} from './common.js';
-import {canvasManager} from '../src/canvasManager.js';
-import {Point} from '../src/lib/geometry.js';
-import {Node, Arrow} from '../src/elements.js';
-import {initAPI, API} from '../src/api.js';
+import { Graph, save, load } from '../src/lib/graph.js';
+import { buildFakeCanvas } from './common.js';
+import { canvasManager } from '../src/canvasManager.js';
+import { Point } from '../src/lib/geometry.js';
+import { Node, Arrow } from '../src/elements.js';
+import { initAPI, API } from '../src/api.js';
+
+let chai = require("chai");
+let chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+let expect = chai.expect;
 
 beforeEach(() => {
-	localStorage = new Map();
-	canvasManager.init(buildFakeCanvas());
-	canvasManager.getInstance().is_external = true;
-	initAPI();
-	API.is_external = true;
+    localStorage = new Map();
+    canvasManager.init(buildFakeCanvas());
+    canvasManager.getInstance().is_external = true;
+    initAPI();
+    API.is_external = true;
 });
 
 afterEach(() => {
-	canvasManager.clear();
+    canvasManager.clear();
 });
 
 
 test('add vertex', () => {
-	let g = new Graph();
-	expect( g.size ).toBe(0);
+    let g = new Graph();
+    expect(g.size).toBe(0);
 
-	g.addVertex(0);
-	expect( g.size ).toBe(1);
-	expect( g.graph.get(0) ).toStrictEqual([]);
+    g.addVertex(0);
+    expect(g.size).toBe(1);
+    expect(g.graph.get(0)).toStrictEqual([]);
 });
 
 
 test('add edge', () => {
-	let g = new Graph();
-	expect( g.size ).toBe(0);
+    let g = new Graph();
+    expect(g.size).toBe(0);
 
-	g.addVertex(0);
-	g.addVertex(1);
-	g.addVertex(2);
+    g.addVertex(0);
+    g.addVertex(1);
+    g.addVertex(2);
 
-	//0->1, 1->2;
-	g.addEdge(0,1);
-	g.addEdge(1,2);
+    //0->1, 1->2;
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
 
-	expect( g.size ).toBe(3);
-	expect( g.graph.get(0) ).toStrictEqual([1]);
-	expect( g.graph.get(1) ).toStrictEqual([2]);
-	expect( g.graph.get(2) ).toStrictEqual([]);
+    expect(g.size).toBe(3);
+    expect(g.graph.get(0)).toStrictEqual([1]);
+    expect(g.graph.get(1)).toStrictEqual([2]);
+    expect(g.graph.get(2)).toStrictEqual([]);
 
 });
 
 
 test('delete vertex', () => {
-	let g = new Graph();
-	g.addVertex(0);
-	g.addVertex(1);
-	g.addVertex(2);
-	g.addVertex(3);
+    let g = new Graph();
+    g.addVertex(0);
+    g.addVertex(1);
+    g.addVertex(2);
+    g.addVertex(3);
 
-	g.addEdge(1,2);
-	g.addEdge(1,3);
-	g.addEdge(3,0);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(3, 0);
 
-	//1->2, 1->3, 3->0
-	g.deleteVertex(2);
-	expect(g.size).toBe(3);
+    //1->2, 1->3, 3->0
+    g.deleteVertex(2);
+    expect(g.size).toBe(3);
 
-	expect( g.graph.get(1) ).toStrictEqual([3]);
-	expect( g.graph.get(2) ).toBe(undefined);
+    expect(g.graph.get(1)).toStrictEqual([3]);
+    expect(g.graph.get(2)).toBe(undefined);
 
-	g.deleteVertex(3);
+    g.deleteVertex(3);
 
-	expect( g.graph.get(1) ).toStrictEqual([]);
-	expect( g.graph.size ).toBe(g.size);
+    expect(g.graph.get(1)).toStrictEqual([]);
+    expect(g.graph.size).toBe(g.size);
 
 });
 
 
 test('delete edge', () => {
-	let g = new Graph();
+    let g = new Graph();
 
-	g.addVertex(0);
-	g.addVertex(1);
-	g.addEdge(0,1);
+    g.addVertex(0);
+    g.addVertex(1);
+    g.addEdge(0, 1);
 
-	expect(g.graph.get(0)).toStrictEqual([1]);
-	g.deleteEdge(0,1);
-	expect(g.graph.get(0)).toStrictEqual([]);
+    expect(g.graph.get(0)).toStrictEqual([1]);
+    g.deleteEdge(0, 1);
+    expect(g.graph.get(0)).toStrictEqual([]);
 });
 
 
 test('save empty', () => {
-	save();
-	let expected = { object_map: "{}", nodes: "[]", arrows: "[]" };
-	
-	expect(localStorage.getItem('object_map')).toBe("{}");
-	expect(localStorage.getItem('nodes')).toBe("[]");
-	expect(localStorage.getItem('arrows')).toBe("[]");
+    save();
+    let expected = { object_map: "{}", nodes: "[]", arrows: "[]" };
+
+    expect(localStorage.getItem('object_map')).toBe("{}");
+    expect(localStorage.getItem('nodes')).toBe("[]");
+    expect(localStorage.getItem('arrows')).toBe("[]");
 
 })
 
 
 test('save node', () => {
-	let CM = canvasManager.getInstance();
-	CM.addNewNode(new Node(new Point(), '1'));
-	save();
-	let copy = CM;
-	CM.clearCanvas();
-	load();
-	
-	for(let i = 0; i < CM.nodes.length; i++){
-		expect(CM.nodes[i]).toEqual(copy.nodes[i]);
-	}
+    let CM = canvasManager.getInstance();
+    CM.addNewNode(new Node(new Point(), '1'));
+    save();
+    let copy = CM;
+    CM.clearCanvas();
+    load();
+
+    for (let i = 0; i < CM.nodes.length; i++) {
+        expect(CM.nodes[i]).toEqual(copy.nodes[i]);
+    }
 
 });
 
 
 test('save arrow', () => {
-	let CM = canvasManager.getInstance();
-	let a = new Node(new Point(), '1');
-	let b = new Node(new Point(), '2');
+    let CM = canvasManager.getInstance();
+    let a = new Node(new Point(), '1');
+    let b = new Node(new Point(), '2');
 
-	CM.addNewNode(a);
-	CM.addNewNode(b);
-	CM.addNewArrow(a,b);
+    CM.addNewNode(a);
+    CM.addNewNode(b);
+    CM.addNewArrow(a, b);
 
-	save();	
-	let copy = canvasManager.getInstance();
+    save();
+    let copy = canvasManager.getInstance();
 
 
-	CM.clearCanvas();
-	load();
+    CM.clearCanvas();
+    load();
 
-	for(let i = 0; i < CM.nodes.length; i++){
-		expect(CM.nodes[i]).toEqual(copy.nodes[i]);
-	}
+    for (let i = 0; i < CM.nodes.length; i++) {
+        expect(CM.nodes[i]).toEqual(copy.nodes[i]);
+    }
 
-	for(let i = 0; i < CM.arrows.length; i++){
-		expect(CM.arrows[i]).toEqual(copy.arrows[i]);
-	}
+    for (let i = 0; i < CM.arrows.length; i++) {
+        expect(CM.arrows[i]).toEqual(copy.arrows[i]);
+    }
 });
 
 
 test('save multiple', () => {
-	let CM = canvasManager.getInstance();
-	let a = new Node(new Point(), '1');
-	let b = new Node(new Point(), '2');
+    let CM = canvasManager.getInstance();
+    let a = new Node(new Point(), '1');
+    let b = new Node(new Point(), '2');
 
-	CM.addNewNode(a);
-	CM.addNewNode(b);
+    CM.addNewNode(a);
+    CM.addNewNode(b);
 
-	CM.addNewArrow(a,b);
+    CM.addNewArrow(a, b);
 
-	for(let times = 0; times < 5; times ++){
-		save();	
-		let copy = CM;
+    for (let times = 0; times < 5; times++) {
+        save();
+        let copy = CM;
 
-		CM.clearCanvas();
+        CM.clearCanvas();
 
-		load();
+        load();
 
-		for(let i = 0; i < CM.nodes.length; i++){
-			expect(CM.nodes[i]).toEqual(copy.nodes[i]);
-		}
+        for (let i = 0; i < CM.nodes.length; i++) {
+            expect(CM.nodes[i]).toEqual(copy.nodes[i]);
+        }
 
-		for(let i = 0; i < CM.arrows.length; i++){
-			expect(CM.arrows[i]).toEqual(copy.arrows[i]);
-		}
-	}
+        for (let i = 0; i < CM.arrows.length; i++) {
+            expect(CM.arrows[i]).toEqual(copy.arrows[i]);
+        }
+    }
 })

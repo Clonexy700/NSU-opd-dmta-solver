0cd2a9dc063370f804044cf8562f1dd479dd21bc
Clonexy <conexy700@gmail.com>
2022-05-15 11:30:39 +0700

    Update simple_fsm.js

--- cypress/integration/simple_fsm.js
+++ cypress/integration/simple_fsm.js
@@ -1,12 +1,1 @@
 //
-describe('Build basic FSS', () => {
-    it('visit app', () => {
-        cy.visit('http://localhost:5000/stateMachine');
-    });
-
-    it('build state', () => {
-        cy.get('canvas').dblclick();
-        cy.get('canvas').click({ shiftKey: true });
-        cy.contains('Нарисовать таблицу').click();
-    });
-});
